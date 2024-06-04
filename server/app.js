fa5a55f698391ef64bdf9f2160101ffabb7871a6
HazimAdel101 <alsaqqafhazim2002@gmail.com>
2024-06-04 06:35:15 -0800

    finish logging with morgan

--- server/app.js
+++ server/app.js
@@ -3,23 +3,34 @@ const session = require('express-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream'); // version 2.x
+const expressLayouts = require('express-ejs-layouts');
 const passportSetup = require('./config/passportSetup.js');
-const expressLoyouts = require('express-ejs-layouts');
 
 const app = express();
 app.use(express.json());
 
+// create a rotating write stream
+const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'log')
+});
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // static files
-
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/css'));
 app.use('/images', express.static(__dirname + 'public/images'));
 app.use('/uploads', express.static(__dirname + 'public/uploads'));
 
-
 app.set('view engine', 'ejs');
+
+// middleware
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -34,21 +45,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(expressLoyouts);
+app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-
-app.get('/collections/:id/edit', async (req, res) => {
-    try {
-        const collection = await Collection.findById(req.params.id);
-        if (!collection) {
-            return res.status(404).send('Collection not found');
-        }
-        res.render('toby', { collection, user: req.user });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
-});
 
 module.exports = app;
